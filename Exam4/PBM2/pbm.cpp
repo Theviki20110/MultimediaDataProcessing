e001957ed483f230097d1801402fb8a795de512b
VINCENZO LAPADULA <267759@unimore.it>
2023-05-14 18:40:14 +0200

    Exam4 (pt.2) almost done but not tested

--- Exam4/PBM2/pbm.cpp
+++ Exam4/PBM2/pbm.cpp
@@ -1,19 +1,28 @@
 #include "pbm.h"
 
+
+void add(Image& img, uint8_t elem) {
+	if (elem == 0)
+		img.ImageData.push_back(0);
+	else
+		img.ImageData.push_back((elem | 255));
+}
+
+
+
 Image BinaryImageToImage(const BinaryImage& bimg)
 {
 	Image img;
+	if ((bimg.H == NULL) || (bimg.W == NULL))
+		return img;
+ 
 	img.H = bimg.H;
 	img.W = bimg.W;
 
 
-	for (int r = 0; r < img.W; r++) {
-		for (int c = 0; c < img.H; c++) {
-			cout << bimg.ImageData[r * img.W + c];
-			if (bimg.ImageData[r * img.W + c] == 0)
-				img.ImageData[r * img.W + c] = 0;
-			else 
-				img.ImageData[r * img.W + c] = ((img.ImageData[r * img.W + c] << 6) | 255);
+	for (int r = 0; r < img.H; r++) {
+		for (int c = 0; c < img.W; c++) {
+			add(img, bimg.ImageData[r * img.W + c]);
 		}
 	}
 
--- Exam4/PBM2/pbm.h
+++ Exam4/PBM2/pbm.h
@@ -10,6 +10,68 @@ struct BinaryImage {
     int W;
     int H;
     std::vector<uint8_t> ImageData;
+
+	bool check_header(ifstream& is, int* width, int* height) {
+
+		string tmp;
+		char c;
+
+		/* Read MagicNumber */
+		is >> tmp;
+
+		if (tmp != "P4")
+			return false;
+
+		/* Read Line Feed */
+		is.get(c);
+
+		/* Read optional comment */
+		if (is.peek() == '#') {
+			is.get(c);
+			string comment;
+			is >> comment;
+
+			/* Read Line Feed */
+			is.get(c);
+		}
+
+		/* Read width and height */
+		is >> *width;
+		is >> *height;
+
+		/* Read Line Feed */
+		is.get(c);
+
+		return true;
+	}
+
+
+	bool ReadFromPBM(const std::string& filename)
+	{
+		ifstream is(filename, ios::binary);
+		if (is.fail())
+			return false;
+
+		int width, height;
+
+		if (!check_header(is, &width, &height))
+			return false;
+
+		this->W = width;
+		this->H = height;
+
+		for (int r = 0; r < height; r++) {
+			for (int c = 0; c < width / 8; c++) {
+				this->ImageData.push_back(is.get());
+			}
+
+			if (width % 8 != 0)
+				this->ImageData.push_back(is.get());
+		}
+
+
+		return true;
+	}
 };
 
 struct Image {
